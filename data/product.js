7444f1c934d6bde863fa08c1b9a67966e79f0945
fhmizany <fhmizany024@icloud.com>
2025-12-26 19:08:20 +0800

    mock data saved

---
+++ data/product.js
@@ -0,0 +1,42 @@
+export const products = [
+{
+  id:331,
+  name:'Textbook Electrical Engineering Circuits',
+  image:'images/products/textbook-engineering.jpg',
+  mahallah:'Mahallah Farouq',
+  condition:'Gred A',
+  priceCents: 4800,
+  rating: {
+      image: 'images/icons/rating-star.jpeg',
+      count:4.5
+    },
+  seller: 'Ahmad Ibrahim'
+} ,
+{
+  id:332,
+  name:'Halfzipper Swag Retro 1990th Design',
+  image:'images/products/georgetown-hoyas-1990s-pullover-jacket.webp',
+  mahallah:'Mahallah Asma',
+  condition:'Gred B',
+  priceCents: 5000,
+  rating: {
+      image: 'images/icons/rating-star.jpeg',
+      count:4.0
+    },
+  seller: 'Dahlia Sahara'
+},
+{
+  id:333,
+  name:'Laptop ASUS ROG 115.G',
+  image:'images/products/ASUS-ROG-LAPTOPS.jpg',
+  mahallah:'Mahallah Bilal',
+  condition:'Gred B',
+  priceCents: 1000000,
+  rating: {
+      image: 'images/icons/rating-star.jpeg',
+      count:4.0
+    },
+  seller: 'Arif Naufal'
+}
+
+];
---
+++ script/marketPlace.js
@@ -0,0 +1,71 @@
+import {products} from '../data/product.js';
+  const sidebar = document.querySelector('.side-navigation');
+  const menuBtn = document.querySelector('.js-menu-btn');
+  const closeBtn = document.querySelector('.js-close-btn');
+
+export function openNav() {
+  sidebar.style.width = '250px'
+
+};
+
+export function closeNav() {
+    sidebar.style.width = '0'
+  };
+
+closeBtn.addEventListener('click',closeNav);
+ menuBtn.addEventListener('click',openNav);
+
+const cart = [];
+
+let productsHTML = '';
+
+products.forEach((product) => {
+  productsHTML += 
+`
+    <div class="product-container js-container-grid">
+        <div class="image-container">
+          <img class="image-products" src=${product.image}>
+        </div>
+        <div class="category-name">
+          ${product.category}
+        </div>
+        
+        <div class="product-name">
+          ${product.name}
+        </div>
+        <div class="seller-info">
+          <div class="seller-name">
+            ${product.seller}
+          </div>
+          <div  class="seller-rating">
+            ${(product.rating.count).toFixed(1)}
+          </div>
+        </div>
+        
+        <div class="product-mahallah">
+            ${product.mahallah}
+        </div>
+        <div class="product-price">
+          RM${priceCentsFixed(product.priceCents)}
+        </div>
+          
+      </div>
+
+`;
+});
+
+document.querySelector('.js-container-grid').innerHTML = productsHTML;
+
+function addToCart(){
+  
+}
+
+export function priceCentsFixed(priceCents){
+  const price = (priceCents / 100).toFixed(2);
+  return price;
+}
+
+
+
+
+
